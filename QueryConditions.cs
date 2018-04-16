f1eba706aaf891225d14c62cdb6dfa73d0acf44a
Laura San Martin <lauragabrielasan@gmail.com>
2018-04-16 13:12:11 -0300

    Add files via upload

---
+++ QueryConditions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LauraStuffs
+{
+    //Create a generic function to be used by the ExpressionPredicateBuilder to create lambda expressions
+    public class QueryConditions
+    {    
+        public object value;
+        public OperatorComparer? comparer;
+        public string[] properties;
+
+        public QueryConditions(object value, OperatorComparer? comparer, params string[] properties)
+        {
+            this.value = value;
+            this.comparer = comparer;
+            this.properties = properties;
+        }
+    }
+}
